3ba5a22e64ea26862d40e7c2411edeefc75eafd4
Miles Baird <mindofmoria@gmail.com>
2024-06-25 19:53:00 -0400

    init
    init

---
+++ projects/ensemble/main.ts
@@ -0,0 +1,12 @@
+
+
+//% color=#0fbc11 icon="\uf0c3"
+namespace custom {
+    /**
+     * Say Hello
+     */
+    //% block="[Category] My Block Name"
+    export function sayHello(): void {
+        // basic.showString("Hello")
+    }
+}
